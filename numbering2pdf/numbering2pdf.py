abe51dfc73d32480bf8464987eee11baca67b0a3
Yingjie Lan <lanyjie@gmail.com>
2024-04-12 15:08:36 +0800

    switch side for even page numbers
    if odd numbered page is on the left/right, then even numbered page will be on the right/left..

--- numbering2pdf/numbering2pdf.py
+++ numbering2pdf/numbering2pdf.py
@@ -12,20 +12,15 @@ standard_fonts = (
     "Symbol", "ZapfDingbats",
 )
 
-position_to_width = {
-    "left": 10,
-    "center": 38,
-    "right": 65.5,
-}
-
-
-def add_numbering_to_pdf(pdf_file, new_pdf_file_path=None, position="center", start_page=1, end_page=None,
+def add_numbering_to_pdf(pdf_file, new_pdf_file_path=None, position="center", 
+                         start_page=1, end_page=None,
                          start_index=1, size=14, font="Times-Roman") -> bytes:
     """Adds numbering to pdf file"""
     pdf_file = get_pdf_file(pdf_file)
     original_pdf = PdfFileReader(BytesIO(pdf_file), strict=False)
-    parameters = get_parameters_for_numbering(original_pdf, position, start_page, end_page, start_index, size, font)
-    empty_numbered_pdf = create_empty_numbered_pdf(**parameters)
+    end_page = end_page or original_pdf.getNumPages() + 1
+    empty_numbered_pdf = create_empty_numbered_pdf(original_pdf, position, 
+                            start_page, end_page, start_index, size, font)
     new_pdf_file = merge_pdf_pages(original_pdf, empty_numbered_pdf)
     if new_pdf_file_path:
         save_file(new_pdf_file, new_pdf_file_path)
@@ -42,42 +37,29 @@ def get_pdf_file(pdf_file) -> bytes:
         file.close()
     return pdf_file
 
-
-def get_parameters_for_numbering(original_pdf, position, start_page, end_page, start_index, size, font) -> dict:
-    """Setting parameters for numbering"""
-    return {
-        "width_of_pages": get_width_of_pages(original_pdf, position),
-        "height": 15 * mm,
-        "start_page": start_page - 1,
-        "end_page": end_page or original_pdf.getNumPages() + 1,
-        "start_index": start_index,
-        "size": size,
-        "font": font,
-        "number_of_pages": original_pdf.getNumPages(),
-    }
-
-
-def get_width_of_pages(original_pdf, position) -> list:
-    """Returns width of pages"""
-    width_of_pages = []
-    for index in range(original_pdf.getNumPages()):
-        ratio = original_pdf.getPage(index).mediaBox.getWidth() / 200
-        width = position_to_width[position] * float(ratio) * mm
-        width_of_pages.append(width)
-    return width_of_pages
-
-
-def create_empty_numbered_pdf(width_of_pages, height, start_page, end_page, start_index, size, font,
-                              number_of_pages) -> PdfFileReader:
+switch_position = {
+    "left": "right",
+    "center": "center",
+    "right": "left"}
+position_to_width = {
+    "left": 10,
+    "center": 38,
+    "right": 65.5}
+def create_empty_numbered_pdf(original_pdf, position, start_page, end_page, 
+                              start_index, size, font) -> PdfFileReader:
     """Returns empty pdf file with numbering only"""
+    number_of_pages = original_pdf.getNumPages()
     empty_canvas = canvas.Canvas("empty_canvas.pdf")
     for index in range(number_of_pages):
+        page_num = index - start_page + start_index
+        number = str(page_num) if start_page<=index<end_page else ""
+        pactual = position if page_num%2 else switch_position[position]
+        pagebox = original_pdf.getPage(index).mediaBox
+        ratio = pagebox.getWidth() / 200 # A4 paper?
+        height = 15 * mm #from bottom? pagebox.getHeight()
+        width_on_page = position_to_width[pactual] * float(ratio) * mm
         empty_canvas.setFont(font, size)
-        if index in range(start_page, end_page):
-            number = str(index - start_page + start_index)
-            empty_canvas.drawString(width_of_pages[index], height, number)
-        else:
-            empty_canvas.drawString(width_of_pages[index], height, "")
+        empty_canvas.drawString(width_on_page, height, number)
         empty_canvas.showPage()
     return PdfFileReader(BytesIO(empty_canvas.getpdfdata()))
 
